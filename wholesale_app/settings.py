2922c7bd28403b7cb98f8af5f8a2cdd79f4198ff
Babs-tunde <smustaphababatunde@gmail.com>
2025-02-22 07:38:13 +0100

    json and setting

--- wholesale_app/settings.py
+++ wholesale_app/settings.py
@@ -137,9 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
-
-# Add this if you have a static directory
+STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfilesbuild', 'static')
 
